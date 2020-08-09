5097205e61a89f7131312328410aee709d053202
admin <oleh@mac.local>
2020-08-10 00:28:25 +0300

    add env, fix home first block

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const app = express();
 
-const ENV = process.ENV || 'development';
+const ENV = process.env.NODE_ENV || 'development';
 
 var transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -21,7 +21,7 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 function rootPath(fileName) {
-    return __dirname + "/" + fileName;
+    return __dirname + "/" + fileName;  
 }
 
 function renderPage(res,pageName) {
